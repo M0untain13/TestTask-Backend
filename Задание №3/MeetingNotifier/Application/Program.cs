66166e14013df2b4bc984ac4420bc27cc83154d4
Danil <dan2002.dan2002@mail.ru>
2024-08-10 17:44:42 +0500

    Добавил пояснения в коментариях

--- Задание №3/MeetingNotifier/Application/Program.cs
+++ Задание №3/MeetingNotifier/Application/Program.cs
@@ -11,20 +11,24 @@ public class Program
 
     private static void Main(string[] args)
     {
+        // Если в аргументах запуска введено "notify", то консоль запустится в режиме получения оповещений о встречах 
         if (args.Length != 0 && args[0] == "notify")
         {
             using var pipeClient = new NamedPipeClientStream(".", _pipeName, PipeDirection.InOut, PipeOptions.None);
             Console.WriteLine("Wait for main application...");
+            // Ждем запуска консоли с основным функционалом
             pipeClient.Connect();
             Console.Clear();
             Console.WriteLine("Meeting Notifications:");
             using var reader = new StreamReader(pipeClient);
+            // До тех пор, пока от главной консоли приходят оповещения, цикл будет продолжаться
             while (pipeClient.IsConnected && !reader.EndOfStream)
             {
                 Console.WriteLine(reader.ReadLine());
             }
             Console.WriteLine("Disconnect");
         }
+        // Иначе консоль запустится с основным функционалом работы со встречами
         else
         {
             var host = CreateHostBuilder(args).Build();
--- Задание №3/MeetingNotifier/Application/Services/NotifyService.cs
+++ Задание №3/MeetingNotifier/Application/Services/NotifyService.cs
@@ -58,6 +58,7 @@ public class NotifyService
         var process = Process.GetCurrentProcess();
         var pipeServer = new NamedPipeServerStream(_pipeName, PipeDirection.InOut, 1);
         Console.WriteLine("Wait for notifications console...");
+        // Ждем запуск консоли, которая будет получать оповещения о встречах
         pipeServer.WaitForConnection();
         Console.Clear();
         Task.Run(async () =>
@@ -65,8 +66,10 @@ public class NotifyService
             using var writer = new StreamWriter(pipeServer);
             _cancellationTokenSource = new CancellationTokenSource();
             var token = _cancellationTokenSource.Token;
+            // Каждые 5 секунд проверяем, подошли время для оповещения о встрече
             while (!token.IsCancellationRequested)
             {
+                // Проверяем только первую встречу, т.к. связный список сортируется по принципу: первая встреча та, о которой нужно раньше всех напомнить
                 var meeting = _meetings.First?.Value;
                 if (meeting is not null && DateTime.Now > meeting.StartDate.Subtract(meeting.ReminderTime))
                 {
@@ -87,6 +90,8 @@ public class NotifyService
         _cancellationTokenSource.Cancel();
     }
 
+    // Метод вызывается только в тех случаях, когда у встреч обновляются такие данные как начало встречи или время до встречи, за которое нужно напомнить
+    // Метод сортирует связный список
     private void OnMeetingPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if(sender is Meeting meeting)
